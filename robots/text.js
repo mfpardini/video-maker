e6a4ed63f71ad60ba14c251bd47ca2f329f6fba3
mfpardini <matheusfpardini@gmail.com>
2020-05-17 13:43:58 -0300

    Add IBM Watson IA to filter keywords

--- robots/text.js
+++ robots/text.js
@@ -2,18 +2,32 @@ const algorithmia = require("algorithmia")
 const augorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const nlu = new NaturalLanguageUnderstandingV1({
+    authenticator: new IamAuthenticator({ apikey: watsonApiKey }),
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+});
 
 async function robot(content) {
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
-    // breakContentIntoSentences(content)
+    breakContentIntoSentences(content)
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     async function fetchContentFromWikipedia(content) {
+        const term = {
+            "articleName": content.searchTerm,
+            "lang": "pt"
+        }
         // modo feito pelo deschamps 
-
         const algorithmiaAuthenticated = algorithmia(augorithmiaApiKey)
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
-        const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+        const wikipediaResponse = await wikipediaAlgorithm.pipe(term)
         const wikipediaContent = wikipediaResponse.get()
         content.sourceContentOriginal = wikipediaContent.content
 
@@ -49,20 +63,56 @@ async function robot(content) {
         function removeDatesInParentheses(text) {
             return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ')
         }
+    }
 
-        function breakContentIntoSentences(content) {
-            content.sentences = []
-            const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
-            sentences.forEach((sentence) => {
-                content.sentences.push({
-                    text: sentence,
-                    keywords: [],
-                    images: []
-                })
+    function breakContentIntoSentences(content) {
+        content.sentences = []
+        const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+        sentences.forEach((sentence) => {
+            content.sentences.push({
+                text: sentence,
+                keywords: [],
+                images: []
             })
+        })
+    }
+
+    function limitMaximumSentences(content) {
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence) {
+        await nlu.analyze(
+            {
+                html: sentence.text,
+                features: {
+                    keywords: {}
+                }
+            })
+            .then(response => {
+                const keywords = response.result.keywords.map(keyword => {
+                    return keyword.text;
+                });
+                console.log(keywords)
+                sentence.keywords = keywords;
+            })
+            .catch(err => {
+                console.log('error: ', err);
+            });
+    }
+
+    async function fetchKeywordsOfAllSentences(content) {
+        const listOfKeywordsToFetch = []
+
+        for (const sentence of content.sentences) {
+            listOfKeywordsToFetch.push(
+                fetchWatsonAndReturnKeywords(sentence)
+            )
         }
 
+        await Promise.all(listOfKeywordsToFetch)
     }
+
 }
 
 module.exports = robot
